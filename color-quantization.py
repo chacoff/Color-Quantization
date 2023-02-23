eca0b0eef86979792d524c66dd675ab18ef4c265
Gomez Chacoff, Jaime <Jaime.Gomez-Chacoff@arcelormittal.com>
2023-02-23 19:16:47 +0100

    contours, hulls and rumpehulls ...

--- color-quantization.py
+++ color-quantization.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import cv2
 import timeit
+import matplotlib.pyplot as plt
 
 
 def kmeans_main(**kwargs):
@@ -34,12 +35,38 @@ def kmeans_main(**kwargs):
     cv2.imwrite('output.png', IMAGE_3D_MATRIX)
     # plt.imshow(IMAGE_3D_MATRIX, cmap='gray')
     # plt.show()
+    return IMAGE_3D_MATRIX
 
+
+def BorderDetectionCanny(border_channel, canvas):
+    im_bw = cv2.threshold(border_channel, 25, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    cv2.imshow('', im_bw)
+    cv2.waitKey(0)
+
+    canny = cv2.Canny(im_bw, 0, 20)  # for border detection
+    # [0] instead of using hierarchy for findContours()
+    contours, hierarchy = cv2.findContours(canny.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # [max(contour, key=cv2.contourArea)]
+    hull = []
+    for i in range(len(contours)):
+        hu = cv2.convexHull(contours[i], returnPoints=False)
+        hull.append(hu)
+        plt.plot(hu)
+        plt.show()
+
+    for j in range(len(contours)):
+        filled_contour = cv2.drawContours(canvas, contours, j,  (255, 255, 255), thickness=cv2.FILLED)
+
+
+    binaryfromcnt = cv2.split(filled_contour)[:3][0]  # splitting in RGB channels
+
+    return binaryfromcnt
 
 if __name__ == "__main__":
 
     # Parameters
-    PATH_TO_FILE = f"Test_Inputs\\02_24.bmp"
+    PATH_TO_FILE = f"Test_Inputs\\02_mod.bmp"
     high = 20  # high threshold in grey levels
     low = 10  # low threshold in grey levels
     ROI_start_y = 900  # ROI start point in y direction
@@ -47,11 +74,34 @@ if __name__ == "__main__":
 
     IMAGE = cv2.imread(PATH_TO_FILE, cv2.IMREAD_UNCHANGED)
     IMAGE = IMAGE[ROI_start_y:(ROI_start_y+ROI_region), 0:IMAGE.shape[1]]  # y:y+h, x:x+w
+
     IMAGE_int = np.array(IMAGE).astype(int)
     print(f'{IMAGE_int.shape = }')
 
     IMAGE_3D_MATRIX = cv2.merge((IMAGE_int, IMAGE_int, IMAGE_int)) if len(IMAGE_int.shape) < 3 else IMAGE_int
+    image_original = IMAGE_3D_MATRIX.copy()  # just to save it
     print(f'{IMAGE_3D_MATRIX.shape = }')
 
-    execution_time = timeit.timeit(f'{kmeans_main()}')
-    sys.exit(f"{1000*execution_time:0.3f} [s]")
+    # execution_time = timeit.timeit(f'{kmeans_main()}')
+    # sys.exit(f"{1000*execution_time:0.3f} [s]")
+
+    image_out = kmeans_main().astype(np.uint8)
+
+    scale_percent = 60  # percent of original size
+    width = int(image_out.shape[1] * scale_percent / 100)
+    height = int(image_out.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    image_out = cv2.resize(image_out, dim, interpolation=cv2.INTER_AREA)
+
+    r, g, b = cv2.split(image_out)[:3]
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+    g = cv2.morphologyEx(g, cv2.MORPH_OPEN, kernel, iterations=1)
+    g = cv2.dilate(g, kernel, iterations=1)
+
+    to_show = BorderDetectionCanny(g, r)
+    cv2.imshow('', to_show)
+    cv2.waitKey()
+
+
